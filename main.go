1f11ebe48dc1aff9b0d4cf0cdeea6c3813cd2f8a
acrossmountain <18339795172@163.com>
2022-05-28 20:50:31 +0800

    feature: Add healthz route

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
+
+	http.ListenAndServe(":12306", nil)
+}
